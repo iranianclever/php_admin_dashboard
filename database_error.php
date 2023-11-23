22af82b3e61fa4d338a549c66f80e3f59ffbd883
iranianclever <iranianclever@gmail.com>
2023-11-23 16:46:09 +0330

    working on database_error.php

---
+++ database_error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>You have error!</title>
+</head>
+<body>
+	<h1>Database Error</h1>
+	<p>Error message: <?php echo $error_message; ?></p>
+</body>
+</html>
